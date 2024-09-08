276d2d74cbe0f138e6b48d3e06398f3ad38cc44d
Claudio Oliveira <dev.caon@gmail.com>
2024-09-08 18:23:41 -0300

    corrigido tamanho dos botões da nav

--- app/components/Navigation/Nav.tsx
+++ app/components/Navigation/Nav.tsx
@@ -15,7 +15,7 @@ const Nav = ({ openNav }: Props) => {
         {/* logo */}
         <div className="flex items-center space-x-2">
           <FaBurger className="w-[1.2rem] h-[1.2rem] sm:w-[1.4rem] sm:h-[1.4rem] text-orange-600" />
-          <h1 className="text-[20px] sm:text-[30px] font-semibold text-blue-950">
+          <h1 className="text-nowrap text-[20px] sm:text-[30px] font-semibold text-blue-950">
             Burger & Cia
           </h1>
         </div>
@@ -43,12 +43,12 @@ const Nav = ({ openNav }: Props) => {
         <div className="flex items-center space-x-4">
           <button className="px-6 py-1 sm:px-8 sm:py-2 text-[14px] sm:text-[16px] bg-blue-950 transition-all duration-200 hover:bg-red-600 flex items-center rounded-md space-x-2 text-white">
             <span>
-              <BiCycling className="w-[1.3rem] h-[1.3rem] sm:w-[1.7rem] sm:h-[1.7rem]" />
+              <BiCycling className="w-[1.3rem] h-[1.3rem] sm:w-[1.5rem] sm:h-[1.5rem]" />
             </span>
             <span className="font-bold">Order Now</span>
           </button>
-          <button className="px-4 py-2 sm:px-6 sm:py-2 hover:bg-green-700 transition-all duration-200 bg-orange-600 flex items-center rounded-md text-white">
-            <BiShoppingBag className="w-[1.3rem] h-[1.3rem] sm:w-[1.7rem] sm:h-[1.7rem]" />
+          <button className="px-6 py-1 sm:px-8 sm:py-2 hover:bg-green-700 transition-all duration-200 bg-orange-600 flex items-center rounded-md text-white">
+            <BiShoppingBag className="w-[1.3rem] h-[1.3rem] sm:w-[1.5rem] sm:h-[1.5rem]" />
           </button>
           <HiMenuAlt2
             onClick={openNav}
